audacity/audacity

Add context menu for clips

src/tracks/playabletrack/wavetrack/ui/WaveTrackView.cpp
@@ -812,6 +812,32 @@ std::weak_ptr<WaveTrackView> WaveTrackSubView::GetWaveTrackView() const
    return mwWaveTrackView;
 }
 
+std::vector<ComponentInterfaceSymbol> WaveTrackSubView::GetMenuItems(
+   const wxRect &rect, const wxPoint *pPosition, AudacityProject *pProject )
+{
+   const WaveClip *pClip = nullptr;
+   auto pTrack = static_cast<WaveTrack*>(FindTrack().get());
+   if (pTrack && pPosition) {
+      auto &viewInfo = ViewInfo::Get(*pProject);
+      auto time = viewInfo.PositionToTime( pPosition->x, rect.x );
+      pClip = pTrack->GetClipAtTime( time );
+   }
+
+   if (pClip)
+      return {
+         { L"Cut", XO("Cut") },
+         { L"Copy", XO("Copy") },
+         { L"Paste", XO("Paste")  },
+         {},
+         { L"Split", XO("Split Clip") },
+         { L"TrackMute", XO("Mute/Unmute Track") },
+         // {},
+         // { L"", XO("Rename clip...") },
+      };
+   else
+      return {};
+}
+
 WaveTrackView &WaveTrackView::Get( WaveTrack &track )
 {
    return static_cast< WaveTrackView& >( TrackView::Get( track ) );

src/tracks/playabletrack/wavetrack/ui/WaveTrackView.h
@@ -52,6 +52,10 @@ protected:
 
    std::weak_ptr<WaveTrackView> GetWaveTrackView() const;
 
+   std::vector<ComponentInterfaceSymbol> GetMenuItems(
+      const wxRect &rect, const wxPoint *pPosition, AudacityProject *pProject )
+   override;
+
 private:
    std::weak_ptr<UIHandle> mCloseHandle;
    std::weak_ptr<UIHandle> mResizeHandle;

tenacityteam/saucedacity

===============================================================================
Cherry picked from Audacity commit
de9f0512db2b4cf1c8e5df3438148d82498c171d. Modified for Saucedacity.
Signed-off-by: Avery King <avery98@pm.me>

src/tracks/playabletrack/wavetrack/ui/WaveTrackView.cpp
@@ -768,6 +768,32 @@ std::weak_ptr<WaveTrackView> WaveTrackSubView::GetWaveTrackView() const
    return mwWaveTrackView;
 }
 
+std::vector<ComponentInterfaceSymbol> WaveTrackSubView::GetMenuItems(
+   const wxRect &rect, const wxPoint *pPosition, SaucedacityProject *pProject )
+{
+   const WaveClip *pClip = nullptr;
+   auto pTrack = static_cast<WaveTrack*>(FindTrack().get());
+   if (pTrack && pPosition) {
+      auto &viewInfo = ViewInfo::Get(*pProject);
+      auto time = viewInfo.PositionToTime( pPosition->x, rect.x );
+      pClip = pTrack->GetClipAtTime( time );
+   }
+
+   if (pClip)
+      return {
+         { L"Cut", XO("Cut") },
+         { L"Copy", XO("Copy") },
+         { L"Paste", XO("Paste")  },
+         {},
+         { L"Split", XO("Split Clip") },
+         { L"TrackMute", XO("Mute/Unmute Track") },
+         // {},
+         // { L"", XO("Rename clip...") },
+      };
+   else
+      return {};
+}
+
 WaveTrackView &WaveTrackView::Get( WaveTrack &track )
 {
    return static_cast< WaveTrackView& >( TrackView::Get( track ) );

src/tracks/playabletrack/wavetrack/ui/WaveTrackView.h
@@ -55,6 +55,10 @@ protected:
 
    std::weak_ptr<WaveTrackView> GetWaveTrackView() const;
 
+   std::vector<ComponentInterfaceSymbol> GetMenuItems(
+      const wxRect &rect, const wxPoint *pPosition, SaucedacityProject *pProject )
+   override;
+
 private:
    std::weak_ptr<UIHandle> mCloseHandle;
    std::weak_ptr<UIHandle> mAdjustHandle;