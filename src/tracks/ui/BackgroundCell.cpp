audacity/audacity

Add context menu for background cell

src/tracks/ui/BackgroundCell.cpp
@@ -149,3 +149,20 @@ wxRect BackgroundCell::DrawingArea(
    else
       return rect;
 }
+
+std::vector<ComponentInterfaceSymbol> BackgroundCell::GetMenuItems(
+   const wxRect &, const wxPoint *, AudacityProject * )
+{
+   // These commands exist in toolbar menus too, but maybe with other labels
+   // TODO: devise a system of registration so that BackgroundCell has no
+   // special knowledge about track sub-types
+   return {
+      { L"NewMonoTrack", XO("Add Mono Track")},
+      { L"NewStereoTrack", XO("Add Stereo Track") },
+      { L"NewLabelTrack", XO("Add Label Track"),  },
+      {},
+      { L"Export", XO("Export Audio..."),  },
+      {},
+      { L"SelectAll", XO("Select All") },
+   };
+}

src/tracks/ui/BackgroundCell.h
@@ -53,6 +53,10 @@ private:
    wxRect DrawingArea(
       TrackPanelDrawingContext &,
       const wxRect &rect, const wxRect &panelRect, unsigned iPass ) override;
+
+   std::vector<ComponentInterfaceSymbol> GetMenuItems(
+      const wxRect &rect, const wxPoint *pPosition, AudacityProject *pProject )
+   override;
    
    AudacityProject *mpProject;
 

tenacityteam/saucedacity

===============================================================================
Cherry picked from Audacity commit
ab7ccfde2b6cce253c255c74bda63fbc3a0d0d34. Modified for Saucedacity.
Signed-off-by: Avery King <avery98@pm.me>

src/tracks/ui/BackgroundCell.cpp
@@ -150,3 +150,20 @@ wxRect BackgroundCell::DrawingArea(
    else
       return rect;
 }
+
+std::vector<ComponentInterfaceSymbol> BackgroundCell::GetMenuItems(
+   const wxRect &, const wxPoint *, SaucedacityProject * )
+{
+   // These commands exist in toolbar menus too, but maybe with other labels
+   // TODO: devise a system of registration so that BackgroundCell has no
+   // special knowledge about track sub-types
+   return {
+      { L"NewMonoTrack", XO("Add Mono Track")},
+      { L"NewStereoTrack", XO("Add Stereo Track") },
+      { L"NewLabelTrack", XO("Add Label Track"),  },
+      {},
+      { L"Export", XO("Export Audio..."),  },
+      {},
+      { L"SelectAll", XO("Select All") },
+   };
+}

src/tracks/ui/BackgroundCell.h
@@ -53,6 +53,10 @@ private:
    wxRect DrawingArea(
       TrackPanelDrawingContext &,
       const wxRect &rect, const wxRect &panelRect, unsigned iPass ) override;
+
+   std::vector<ComponentInterfaceSymbol> GetMenuItems(
+      const wxRect &rect, const wxPoint *pPosition, SaucedacityProject *pProject )
+   override;
    
    SaucedacityProject *mpProject;
 