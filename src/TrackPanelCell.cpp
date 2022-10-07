audacity/audacity

Exit editing of clip name and redraw when TrackPanel loses focus

src/TrackPanelCell.cpp
@@ -79,3 +79,8 @@ unsigned TrackPanelCell::Char(
    event.Skip();
    return RefreshCode::RefreshNone;
 }
+
+unsigned TrackPanelCell::LoseFocus(AudacityProject *)
+{
+   return RefreshCode::RefreshNone;
+}

src/TrackPanelCell.h
@@ -134,6 +134,11 @@ public:
    virtual unsigned Char
       (wxKeyEvent & event, ViewInfo &viewInfo, wxWindow *pParent,
        AudacityProject *project);
+
+   // Return value is a bitwise OR of RefreshCode values
+   // Notification to the focused cell that the CellularPanel is losing focus
+   // Default does nothing, returns RefreshCode::RefreshNone
+   virtual unsigned LoseFocus(AudacityProject *project);
 };
 
 #endif

src/tracks/playabletrack/wavetrack/ui/WaveTrackAffordanceControls.cpp
@@ -114,6 +114,10 @@ public:
     WaveClipTitleEditHandle(const std::shared_ptr<TextEditHelper>& helper)
         : mHelper(helper)
     { }
+   
+   ~WaveClipTitleEditHandle()
+   {
+   }
 
     Result Click(const TrackPanelMouseEvent& event, AudacityProject* project) override
     {
@@ -423,6 +427,11 @@ unsigned WaveTrackAffordanceControls::Char(wxKeyEvent& event, ViewInfo& viewInfo
     return RefreshCode::RefreshNone;
 }
 
+unsigned WaveTrackAffordanceControls::LoseFocus(AudacityProject *)
+{
+   return ExitTextEditing();
+}
+
 void WaveTrackAffordanceControls::OnTextEditFinished(AudacityProject* project, const wxString& text)
 {
     if (auto lock = mFocusClip.lock())
@@ -454,7 +463,12 @@ void WaveTrackAffordanceControls::OnTextContextMenu(AudacityProject* project, co
 void WaveTrackAffordanceControls::OnTrackChanged(TrackListEvent& evt)
 {
     evt.Skip();
+    ExitTextEditing();
+}
 
+unsigned WaveTrackAffordanceControls::ExitTextEditing()
+{
+    using namespace RefreshCode;
     if (mTextEditHelper)
     {
         auto trackList = FindTrack()->GetOwner();
@@ -463,7 +477,9 @@ void WaveTrackAffordanceControls::OnTrackChanged(TrackListEvent& evt)
             mTextEditHelper->Finish(trackList->GetOwner());
         }
         mTextEditHelper.reset();
+        return RefreshCell;
     }
+    return RefreshNone;
 }
 
 

src/tracks/playabletrack/wavetrack/ui/WaveTrackAffordanceControls.h
@@ -72,6 +72,8 @@ public:
     (wxKeyEvent& event, ViewInfo& viewInfo, wxWindow* pParent,
         AudacityProject* project) override;
 
+    unsigned LoseFocus(AudacityProject *project) override;
+
     void OnTextEditFinished(AudacityProject* project, const wxString& text) override;
     void OnTextEditCancelled(AudacityProject* project) override;
     void OnTextModified(AudacityProject* project, const wxString& text) override;
@@ -84,6 +86,8 @@ public:
 
 private:
     void OnTrackChanged(TrackListEvent& evt);
+
+    unsigned ExitTextEditing();
 
     bool SelectNextClip(ViewInfo& viewInfo, AudacityProject* project, bool forward);
 

src/tracks/playabletrack/wavetrack/ui/WaveTrackView.cpp
@@ -1226,6 +1226,16 @@ unsigned WaveTrackView::Char(wxKeyEvent& event, ViewInfo& viewInfo, wxWindow* pP
    return result;
 }
 
+unsigned WaveTrackView::LoseFocus(AudacityProject *project)
+{
+   unsigned result = RefreshCode::RefreshNone;
+   if (auto delegate = mKeyEventDelegate.lock()) {
+      result = delegate->LoseFocus(project);
+      mKeyEventDelegate.reset();
+   }
+   return result;
+}
+
 std::vector< std::shared_ptr< WaveTrackSubView > >
 WaveTrackView::GetAllSubViews()
 {

src/tracks/playabletrack/wavetrack/ui/WaveTrackView.h
@@ -172,6 +172,8 @@ public:
    (wxKeyEvent& event, ViewInfo& viewInfo, wxWindow* pParent,
        AudacityProject* project) override;
 
+   unsigned LoseFocus(AudacityProject *project) override;
+
 private:
    void BuildSubViews() const;
    void DoSetDisplay(Display display, bool exclusive = true);

tenacityteam/saucedacity

===============================================================================
Cherry-picked from Audacity commit ec258e083. Modified for Saucedacity.
Signed-off-by: Avery King <avery98@pm.me>

src/TrackPanelCell.cpp
@@ -79,3 +79,8 @@ unsigned TrackPanelCell::Char(
    event.Skip();
    return RefreshCode::RefreshNone;
 }
+
+unsigned TrackPanelCell::LoseFocus(SaucedacityProject *)
+{
+   return RefreshCode::RefreshNone;
+}

src/TrackPanelCell.h
@@ -134,6 +134,11 @@ public:
    virtual unsigned Char
       (wxKeyEvent & event, ViewInfo &viewInfo, wxWindow *pParent,
        SaucedacityProject *project);
+
+   // Return value is a bitwise OR of RefreshCode values
+   // Notification to the focused cell that the CellularPanel is losing focus
+   // Default does nothing, returns RefreshCode::RefreshNone
+   virtual unsigned LoseFocus(SaucedacityProject *project);
 };
 
 #endif

src/tracks/playabletrack/wavetrack/ui/WaveTrackAffordanceControls.cpp
@@ -119,6 +119,10 @@ public:
     WaveClipTitleEditHandle(const std::shared_ptr<TextEditHelper>& helper)
         : mHelper(helper)
     { }
+   
+   ~WaveClipTitleEditHandle()
+   {
+   }
 
     Result Click(const TrackPanelMouseEvent& event, SaucedacityProject* project) override
     {
@@ -438,6 +442,11 @@ unsigned WaveTrackAffordanceControls::Char(wxKeyEvent& event, ViewInfo& viewInfo
     return RefreshCode::RefreshNone;
 }
 
+unsigned WaveTrackAffordanceControls::LoseFocus(SaucedacityProject *)
+{
+   return ExitTextEditing();
+}
+
 void WaveTrackAffordanceControls::OnTextEditFinished(SaucedacityProject* project, const wxString& text)
 {
     if (auto lock = mFocusClip.lock())
@@ -468,6 +477,13 @@ void WaveTrackAffordanceControls::OnTextContextMenu(SaucedacityProject* project,
 
 void WaveTrackAffordanceControls::OnTrackChanged(TrackListEvent& evt)
 {
+    evt.Skip();
+    ExitTextEditing();
+}
+
+unsigned WaveTrackAffordanceControls::ExitTextEditing()
+{
+    using namespace RefreshCode;
     if (mTextEditHelper)
     {
         auto trackList = FindTrack()->GetOwner();
@@ -476,7 +492,9 @@ void WaveTrackAffordanceControls::OnTrackChanged(TrackListEvent& evt)
             mTextEditHelper->Finish(trackList->GetOwner());
         }
         mTextEditHelper.reset();
+        return RefreshCell;
     }
+    return RefreshNone;
 }
 
 

src/tracks/playabletrack/wavetrack/ui/WaveTrackAffordanceControls.h
@@ -71,6 +71,8 @@ public:
     (wxKeyEvent& event, ViewInfo& viewInfo, wxWindow* pParent,
         SaucedacityProject* project) override;
 
+    unsigned LoseFocus(SaucedacityProject *project) override;
+
     void OnTextEditFinished(SaucedacityProject* project, const wxString& text) override;
     void OnTextEditCancelled(SaucedacityProject* project) override;
     void OnTextModified(SaucedacityProject* project, const wxString& text) override;
@@ -83,6 +85,8 @@ public:
 
 private:
     void OnTrackChanged(TrackListEvent& evt);
+
+    unsigned ExitTextEditing();
 
     bool SelectNextClip(ViewInfo& viewInfo, SaucedacityProject* project, bool forward);
 

src/tracks/playabletrack/wavetrack/ui/WaveTrackView.cpp
@@ -1164,6 +1164,16 @@ unsigned WaveTrackView::Char(wxKeyEvent& event, ViewInfo& viewInfo, wxWindow* pP
    return result;
 }
 
+unsigned WaveTrackView::LoseFocus(SaucedacityProject *project)
+{
+   unsigned result = RefreshCode::RefreshNone;
+   if (auto delegate = mKeyEventDelegate.lock()) {
+      result = delegate->LoseFocus(project);
+      mKeyEventDelegate.reset();
+   }
+   return result;
+}
+
 std::vector< std::shared_ptr< WaveTrackSubView > >
 WaveTrackView::GetAllSubViews()
 {

src/tracks/playabletrack/wavetrack/ui/WaveTrackView.h
@@ -171,6 +171,8 @@ public:
    (wxKeyEvent& event, ViewInfo& viewInfo, wxWindow* pParent,
        SaucedacityProject* project) override;
 
+   unsigned LoseFocus(SaucedacityProject *project) override;
+
 private:
    void BuildSubViews() const;
    void DoSetDisplay(Display display, bool exclusive = true);