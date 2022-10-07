audacity/audacity

Direct text editing via TextEditHelper

src/tracks/playabletrack/wavetrack/ui/WaveTrackAffordanceControls.h
@@ -11,18 +11,36 @@
 #pragma once
 
 #include <wx/font.h>
+#include <wx/event.h>
+
 #include "../../../ui/CommonTrackPanelCell.h"
+#include "../../../ui/TextEditHelper.h"
+
+
+struct TrackListEvent;
 
 class AffordanceHandle;
 class WaveClip;
 class TrackPanelResizeHandle;
+class WaveClipTitleEditHandle;
 class WaveTrackAffordanceHandle;
+class TrackList;
 
-class AUDACITY_DLL_API WaveTrackAffordanceControls : public CommonTrackCell
+//Handles clip movement, selection, navigation and
+//allow name change
+class AUDACITY_DLL_API WaveTrackAffordanceControls : 
+    public CommonTrackCell,
+    public TextEditDelegate,
+    public wxEvtHandler,
+    public std::enable_shared_from_this<WaveTrackAffordanceControls>
 {
     std::weak_ptr<WaveClip> mFocusClip;
     std::weak_ptr<WaveTrackAffordanceHandle> mAffordanceHandle;
     std::weak_ptr<TrackPanelResizeHandle> mResizeHandle;
+    std::weak_ptr<WaveClipTitleEditHandle> mTitleEditHandle;
+
+    std::shared_ptr<TextEditHelper> mTextEditHelper;
+
     wxFont mClipNameFont;
 
 public:
@@ -38,6 +56,7 @@ public:
     bool StartEditClipName(AudacityProject* project);
 
     std::weak_ptr<WaveClip> GetSelectedClip() const;
+
     unsigned CaptureKey
     (wxKeyEvent& event, ViewInfo& viewInfo, wxWindow* pParent,
         AudacityProject* project) override;
@@ -45,8 +64,20 @@ public:
     unsigned KeyDown (wxKeyEvent& event, ViewInfo& viewInfo, wxWindow* pParent,
         AudacityProject* project) override;
 
+    unsigned Char
+    (wxKeyEvent& event, ViewInfo& viewInfo, wxWindow* pParent,
+        AudacityProject* project) override;
+
+    void OnTextEditFinished(AudacityProject* project, const wxString& text) override;
+    void OnTextEditCancelled(AudacityProject* project) override;
+    void OnTextModified(AudacityProject* project, const wxString& text) override;
+    void OnTextContextMenu(AudacityProject* project, const wxPoint& position) override;
+
 private:
+    void OnTrackChanged(TrackListEvent& evt);
 
     bool SelectNextClip(ViewInfo& viewInfo, AudacityProject* project, bool forward);
     bool StartEditSelectedClipName(ViewInfo& viewInfo, AudacityProject* project);
+
+    std::shared_ptr<TextEditHelper> MakeTextEditHelper(const wxString& text);
 };

tenacityteam/saucedacity

===============================================================================
Cherry picked from Audacity commit
4210231957c540ee16d9dbfeccde4b3ca86a1751. Modified for Saucedacity.
Signed-off-by: Avery King <avery98@pm.me>

src/tracks/playabletrack/wavetrack/ui/WaveTrackAffordanceControls.h
@@ -11,18 +11,36 @@
 #pragma once
 
 #include <wx/font.h>
+#include <wx/event.h>
+
 #include "../../../ui/CommonTrackPanelCell.h"
+#include "../../../ui/TextEditHelper.h"
+
+
+struct TrackListEvent;
 
 class AffordanceHandle;
 class WaveClip;
 class TrackPanelResizeHandle;
+class WaveClipTitleEditHandle;
 class WaveTrackAffordanceHandle;
+class TrackList;
 
-class SAUCEDACITY_DLL_API WaveTrackAffordanceControls : public CommonTrackCell
+//Handles clip movement, selection, navigation and
+//allow name change
+class SAUCEDACITY_DLL_API WaveTrackAffordanceControls : 
+    public CommonTrackCell,
+    public TextEditDelegate,
+    public wxEvtHandler,
+    public std::enable_shared_from_this<WaveTrackAffordanceControls>
 {
     std::weak_ptr<WaveClip> mFocusClip;
     std::weak_ptr<WaveTrackAffordanceHandle> mAffordanceHandle;
     std::weak_ptr<TrackPanelResizeHandle> mResizeHandle;
+    std::weak_ptr<WaveClipTitleEditHandle> mTitleEditHandle;
+
+    std::shared_ptr<TextEditHelper> mTextEditHelper;
+
     wxFont mClipNameFont;
 
 public:
@@ -38,6 +56,7 @@ public:
     bool StartEditClipName(SaucedacityProject* project);
 
     std::weak_ptr<WaveClip> GetSelectedClip() const;
+
     unsigned CaptureKey
     (wxKeyEvent& event, ViewInfo& viewInfo, wxWindow* pParent,
         SaucedacityProject* project) override;
@@ -45,8 +64,20 @@ public:
     unsigned KeyDown (wxKeyEvent& event, ViewInfo& viewInfo, wxWindow* pParent,
         SaucedacityProject* project) override;
 
+    unsigned Char
+    (wxKeyEvent& event, ViewInfo& viewInfo, wxWindow* pParent,
+        SaucedacityProject* project) override;
+
+    void OnTextEditFinished(SaucedacityProject* project, const wxString& text) override;
+    void OnTextEditCancelled(SaucedacityProject* project) override;
+    void OnTextModified(SaucedacityProject* project, const wxString& text) override;
+    void OnTextContextMenu(SaucedacityProject* project, const wxPoint& position) override;
+
 private:
+    void OnTrackChanged(TrackListEvent& evt);
 
     bool SelectNextClip(ViewInfo& viewInfo, SaucedacityProject* project, bool forward);
     bool StartEditSelectedClipName(ViewInfo& viewInfo, SaucedacityProject* project);
+
+    std::shared_ptr<TextEditHelper> MakeTextEditHelper(const wxString& text);
 };